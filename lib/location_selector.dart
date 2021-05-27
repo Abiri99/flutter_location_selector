e810bd02ebc315168f76a390fd969c8d6a1add51
alireza <abiriali78@gmail.com>
2021-05-27 15:19:59 +0430

    feat: added america continent and ocean color background

---
+++ lib/location_selector.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_country_selector/world_map_painter.dart';
+
+class LocationSelector extends StatelessWidget {
+  const LocationSelector({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Center(
+            child: AspectRatio(
+              aspectRatio: 800 / 520,
+              child: Image.asset(
+                'asset/world-map-simple.jpg', // image size: 800x520
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          Center(
+            child: AspectRatio(
+              aspectRatio: 800 / 520,
+              child: ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(1000)),
+                child: CustomPaint(
+                  painter: WorldMapPainter(),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/world_map_painter.dart
@@ -0,0 +1,190 @@
+import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
+
+class WorldMapPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.clipRRect(
+      RRect.fromRectAndRadius(
+        Rect.fromCircle(
+          center: Offset(size.width / 2, size.height / 2),
+          radius: size.height / 2,
+        ),
+        Radius.circular(10000),
+      ),
+    );
+    _drawBackgroundColor(canvas, size);
+
+    // canvas.drawColor(Colors.blue, BlendMode.src);
+    canvas.translate(0, 0);
+
+    _drawAmericaContinentShadow(canvas);
+    _drawAmericaContinent(canvas);
+    _drawAsiaContinent(canvas);
+  }
+
+  void _drawBackgroundColor(Canvas canvas, Size size) {
+    Path path = Path();
+    path.lineTo(size.width, 0);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.close();
+
+    Paint painter = Paint()
+      ..shader = ui.Gradient.radial(
+        Offset(4 * size.width / 5, 3 * size.height / 4),
+        300,
+        [
+          // Color(0xff003d60),
+          // Color(0xff08ab7),
+          // Color(0xff009eb5),
+          // Color(0xff00aa99),
+          // Color(0xff009081),
+
+          Color(0xff3394CC),
+          Color(0xff68B0E0),
+          // Color(0xff8CC1E6),
+        ],
+        [
+          // 0.3,
+          0.3,
+          // 0.4,
+          // 0.9,
+          // 0.8,
+          1.0,
+        ],
+      );
+    // ..blendMode = BlendMode.multiply;
+
+    canvas.drawPath(path, painter);
+  }
+
+  void _drawAsiaContinent(Canvas canvas) {
+    Path path = Path();
+
+    // cubicTo(canvas, path, x1, y1, x2, y2, x3, y3)
+
+    Paint paint = Paint()
+      ..color = Colors.blue
+      ..strokeWidth = 3.0
+      ..strokeJoin = StrokeJoin.round
+      ..strokeCap = StrokeCap.round
+      ..isAntiAlias = true
+      ..style = PaintingStyle.fill;
+
+    canvas.drawCircle(Offset(205, 180), 2, paint);
+
+    canvas.drawPath(path, paint);
+  }
+
+  void _drawAmericaContinent(Canvas canvas) {
+    Path path = Path();
+    // draw America
+    path.moveTo(60, 60);
+    path.quadraticBezierTo(42, 60, 38, 72);
+    path.quadraticBezierTo(40, 80, 50, 73);
+    path.quadraticBezierTo(73, 62, 62, 90);
+    path.quadraticBezierTo(60, 90, 65, 100);
+    path.quadraticBezierTo(68, 105, 69, 115);
+    path.quadraticBezierTo(70, 122, 80, 122);
+    path.quadraticBezierTo(85, 122, 89, 132);
+    path.quadraticBezierTo(92, 138, 98, 138);
+    path.quadraticBezierTo(100, 142, 97, 148);
+    path.quadraticBezierTo(95, 156, 102, 160);
+    path.quadraticBezierTo(95, 156, 102, 160);
+    path.cubicTo(118, 170, 100, 190, 122, 210);
+    path.cubicTo(115, 200, 130, 190, 129, 185);
+    path.cubicTo(127, 183, 158, 150, 137, 148);
+    path.cubicTo(125, 148, 120, 122, 102, 130);
+    path.cubicTo(85, 132, 98, 125, 91, 118);
+    path.cubicTo(91, 110, 98, 118, 101, 103);
+    path.cubicTo(102, 94, 120, 89, 118, 89);
+    path.cubicTo(152, 82, 124, 80, 132, 68);
+    path.cubicTo(133, 66, 128, 64, 132, 58);
+    path.cubicTo(148, 52, 135, 70, 145, 70);
+    cubicTo(canvas, path, 148, 72, 158, 59, 172, 55, drawCircles: false);
+    cubicTo(canvas, path, 180, 34, 155, 54, 140, 45, drawCircles: false);
+    cubicTo(canvas, path, 130, 40, 125, 58, 110, 51, drawCircles: false);
+    cubicTo(canvas, path, 100, 45, 100, 70, 60, 60, drawCircles: false);
+    path.close();
+
+    Paint paint = Paint()
+      ..color = Color(0xffF9AF66)
+      // ..color = Colors.brown
+      ..strokeWidth = 3.0
+      ..strokeJoin = StrokeJoin.round
+      ..strokeCap = StrokeCap.round
+      ..isAntiAlias = true
+      ..style = PaintingStyle.fill;
+
+    canvas.drawPath(path, paint);
+  }
+
+  void _drawAmericaContinentShadow(Canvas canvas) {
+    Path path = Path();
+    // draw America
+    path.moveTo(60, 60);
+    path.quadraticBezierTo(42, 60, 38, 72);
+    path.quadraticBezierTo(40, 80, 50, 73);
+    path.quadraticBezierTo(73, 62, 62, 90);
+    path.quadraticBezierTo(60, 90, 65, 100);
+    path.quadraticBezierTo(68, 105, 69, 115);
+    path.quadraticBezierTo(70, 122, 80, 122);
+    path.quadraticBezierTo(85, 122, 89, 132);
+    path.quadraticBezierTo(92, 138, 98, 138);
+    path.quadraticBezierTo(100, 142, 97, 148);
+    path.quadraticBezierTo(95, 156, 102, 160);
+    path.quadraticBezierTo(95, 156, 102, 160);
+    path.cubicTo(118, 170, 100, 190, 122, 210);
+    path.cubicTo(115, 200, 130, 190, 129, 185);
+    path.cubicTo(127, 183, 158, 150, 137, 148);
+    path.cubicTo(125, 148, 120, 122, 102, 130);
+    path.cubicTo(85, 132, 98, 125, 91, 118);
+    path.cubicTo(91, 110, 98, 118, 101, 103);
+    path.cubicTo(102, 94, 120, 89, 118, 89);
+    path.cubicTo(152, 82, 124, 80, 132, 68);
+    path.cubicTo(133, 66, 128, 64, 132, 58);
+    path.cubicTo(148, 52, 135, 70, 145, 70);
+    cubicTo(canvas, path, 148, 72, 158, 59, 172, 55, drawCircles: false);
+    cubicTo(canvas, path, 180, 34, 155, 54, 140, 45, drawCircles: false);
+    cubicTo(canvas, path, 130, 40, 125, 58, 110, 51, drawCircles: false);
+    cubicTo(canvas, path, 100, 45, 100, 70, 60, 60, drawCircles: false);
+    path.close();
+
+    Paint paint = Paint()
+      ..color = Colors.black26
+      ..imageFilter = ui.ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0)
+    // ..color = Colors.brown
+      ..isAntiAlias = true
+      ..style = PaintingStyle.fill;
+
+    canvas.drawPath(path, paint);
+  }
+
+  void cubicTo(
+    Canvas canvas,
+    Path path,
+    double x1,
+    double y1,
+    double x2,
+    double y2,
+    double x3,
+    double y3, {
+    bool drawCircles = true,
+  }) {
+    if (drawCircles) {
+      Paint circlePainter = Paint()
+        ..style = PaintingStyle.fill
+        ..color = Colors.blue;
+      canvas.drawCircle(Offset(x1, y1), 3, circlePainter);
+      canvas.drawCircle(Offset(x2, y2), 3, circlePainter);
+      canvas.drawCircle(Offset(x3, y3), 3, circlePainter);
+    }
+
+    path.cubicTo(x1, y1, x2, y2, x3, y3);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) =>
+      this != oldDelegate;
+}
